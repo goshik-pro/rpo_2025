63c438f508bdef94aaa1fed7ca69e8185d65279f
goshik-pro <85579926+goshik-pro@users.noreply.github.com>
2025-06-16 01:31:18 +0300

    2

---
+++ Downloads/рпо/RPO-2/app/src/main/java/org/ui3/fclient/MainActivity.java
@@ -0,0 +1,103 @@
+package org.ui3.fclient;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
+
+import org.ui3.fclient.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'fclient' library on application startup.
+    static {
+        System.loadLibrary("fclient");
+        System.loadLibrary("mbedcrypto");
+
+    }
+
+    private ActivityMainBinding binding;
+    ActivityResultLauncher activityResultLauncher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        int res = initRng();
+        byte[] v = randomBytes(10);
+
+        byte[] key = new byte[16];
+        byte[] data = "Hello world".getBytes();
+        byte[] encryptedData = encrypt(key, data);
+        byte[] decryptedData = decrypt(key, encryptedData);
+        Log.d("decryptedData", new String(decryptedData));
+
+        // Example of a call to a native method
+//        TextView tv = binding.sampleText;
+//        tv.setText(stringFromJNI());
+
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            // Обработка результата
+                            if (data != null) {
+                                String pin = data.getStringExtra("pin");
+                                Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+    }
+
+    /**
+     * A native method that is implemented by the 'fclient' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+    public native int initRng();
+    public static native byte[] randomBytes(int no);
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
+
+    public static byte[] stringToHex(String s)
+    {
+        byte[] hex;
+        try
+        {
+            hex = Hex.decodeHex(s.toCharArray());
+        }
+        catch (DecoderException ex)
+        {
+            hex = null;
+        }
+        return hex;
+    }
+
+    public void onButtonClick(View v)
+    {
+        Intent it = new Intent(this, PinpadActivity.class);
+        //startActivity(it);
+        activityResultLauncher.launch(it);
+    }
+
+}
---
+++ Downloads/рпо/RPO-2/libs/mbedtls/build/compile.bat
@@ -0,0 +1,17 @@
+
+::set ABI=armaebi-v7a
+::set ABI=x86
+::set ABI=arm64-v8a
+set ABI=x86_64
+
+set ANDROID_NDK=D:\ProgramFiles\Android\sdk\ndk\27.0.12077973
+set TOOL_CHAIN=%ANDROID_NDK%\build\cmake\android.toolchain.cmake
+set CMAKE=D:\ProgramFiles\Android\sdk\cmake\3.22.1\bin\cmake.exe
+set PATH=%PATH%;D:\ProgramFiles\Android\sdk\cmake\3.22.1\bin
+
+mkdir %ABI%
+cd %ABI%
+
+%CMAKE% -GNinja ../../mbedtls -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 -DANDROID_ABI=%ABI% -DCMAKE_TOOLCHAIN_FILE=%TOOL_CHAIN% -DUSE_SHARED_MBEDTLS_LIBRARY=On -DENABLE_TESTING=Off
+
+%CMAKE% --build .
