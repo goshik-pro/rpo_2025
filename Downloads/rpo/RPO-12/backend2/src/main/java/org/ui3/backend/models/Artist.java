d016f781cf86e21d2cf6b9fdd78c67ccba5e6b73
goshik-pro <85579926+goshik-pro@users.noreply.github.com>
2025-06-16 10:34:39 +0300

    k

---
+++ Downloads/rpo/RPO-12/backend2/src/main/java/org/ui3/backend/models/Artist.java
@@ -0,0 +1,29 @@
+package org.ui3.backend.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "artists")
+@Access(AccessType.FIELD)
+public class Artist {
+
+    public Artist(){ }
+    public Artist(Long id){
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    public Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    public String name;
+
+    @Column(name = "century", nullable = false)
+    public int age;
+
+    @ManyToOne()
+    @JoinColumn(name = "countryid")
+    public Country country;
+}
---
+++ Downloads/rpo/RPO-12/backend2/src/main/java/org/ui3/backend/tools/DataValidationException.java
@@ -0,0 +1,4 @@
+package org.ui3.backend.tools;
+
+public class DataValidationException {
+}
